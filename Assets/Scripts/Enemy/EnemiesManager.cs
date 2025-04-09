bba9b9a6a6a8f62af6f0146dc052c5d62b1e3e6e
Joshua <joshloufek@gmail.com>
2025-04-09 13:02:05 -0400

    Update EnemiesManager.cs

--- Assets/Scripts/Enemy/EnemiesManager.cs
+++ Assets/Scripts/Enemy/EnemiesManager.cs
@@ -41,28 +41,38 @@ public class EnemiesManager : MonoBehaviour
 
     public void SpawnEnemy()
     {
-        float x = 0;
-        float y = 0;
-
-        // Determines the spawning coordinates of the enemy and ensures it's outside of the player's view on the x or y axis.
-        if ((Random.Range(0, 2) == 0)) { // This randomly chooses the x or the y to be outside the player's vision
-            x = spawnRect.x * (Random.Range(0, 2) * 2 - 1); // Locked outside player vision
-            y = UnityEngine.Random.Range(-spawnRect.y, spawnRect.y); // Can dynamically place on this axis
-        } else {
-            x = UnityEngine.Random.Range(-spawnRect.x, spawnRect.x); // Can dynamically place on this axis
-            y = spawnRect.y * (Random.Range(0, 2) * 2 - 1); // Locked outside player vision
-        }   
-
-        // Sets up the vector for enemies to spawn on
-        Vector3 position = new Vector3(x, y, 0f);
-        
-        // Sets the enemy's position to be relative to the player
-        position += player.transform.position; 
+        Vector3 position = GenerateRandomPosition();
 
         // Create the enemy and set it to follow and attack the player
         GameObject newEnemy = Instantiate(enemy); // Create the enemy
         newEnemy.GetComponent<Enemy>().InitializeEnemy(position, player, this); // Initialize the enemy scripts
         newEnemy.transform.parent = transform; // Set's the enemy to be a child of this object. Keeps the scene hierarchy clean.
+    }
+
+    private Vector3 GenerateRandomPosition()
+    {
+        float x = 0;
+        float y = 0;
+
+        // Determines the spawning coordinates of the enemy and ensures it's outside of the player's view on the x or y axis.
+        if ((Random.Range(0, 2) == 0))
+        { // This randomly chooses the x or the y to be outside the player's vision
+            x = spawnRect.x * (Random.Range(0, 2) * 2 - 1); // Locked outside player vision
+            y = UnityEngine.Random.Range(-spawnRect.y, spawnRect.y); // Can dynamically place on this axis
+        }
+        else
+        {
+            x = UnityEngine.Random.Range(-spawnRect.x, spawnRect.x); // Can dynamically place on this axis
+            y = spawnRect.y * (Random.Range(0, 2) * 2 - 1); // Locked outside player vision
+        }
+
+        // Sets up the vector for enemies to spawn on
+        Vector3 position = new Vector3(x, y, 0f);
+
+        // Sets the enemy's position to be relative to the player
+        position += player.transform.position;
+
+        return position;
     }
 
     public void EnemyDied()
