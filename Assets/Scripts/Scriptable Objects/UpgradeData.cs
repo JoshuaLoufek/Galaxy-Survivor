cb9216acf89f42ec2b55f68b30c9cc6f4e1213e4
Joshua <joshloufek@gmail.com>
2025-04-14 09:42:44 -0400

    LaserGun Full Upgrade Path
    Implemented a full set of 10 upgrades for the laser gun.

--- Assets/Scripts/Scriptable Objects/UpgradeData.cs
+++ Assets/Scripts/Scriptable Objects/UpgradeData.cs
@@ -15,13 +15,18 @@ public enum UpgradeType
 [CreateAssetMenu]
 public class UpgradeData : ScriptableObject
 {
+    // Universal Upgrade Variables
     public UpgradeType upgradeType; // How the player Level script should interpret this upgrade
     public string Name; // The name of the upgrade
     public string description; // A description of the upgrade
     public int level; // The level the player needs to be for this upgrade to be avaliable to them
     public Sprite icon; // How the upgrade is visually represented to the player
+    
+    // Weapon Specific
     public WeaponData weaponData; // Holds a reference to the weapon that this upgrade belongs to.
     public WeaponStats weaponUpgradeStats; // Holds the actual upgrade stats
+
+    // Also Universal, but deals specifically with what happens after this upgrade is chosen
     public List<UpgradeData> addWhenChosen; // Upgrade(s) that are added to the list when this upgrade is chosen.
     public List<UpgradeData> removeWhenChosen; // Upgrade(s) that should be removed from the list when this upgrade is chosen.
 }
